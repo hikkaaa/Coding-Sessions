8bccda10bbdaa4ea202b720b8b40442d7dfa84b6
hikkaaa <92394378+hikkaaa@users.noreply.github.com>
2022-04-29 18:04:14 +0200

    Add files via upload

--- sample_madlibs/hp.py
+++ sample_madlibs/hp.py
@@ -1,23 +1,40 @@
 def madlib():
-    body_part = input("Body Part: ")
-    verb = input("Verb: ")
     adj1 = input("Adjective: ")
     adj2 = input("Adjective: ")
     adj3 = input("Adjective: ")
     adj4 = input("Adjective: ")
-    adj5 = input("Adjective: ")
     noun1 = input("Noun: ")
     noun2 = input("Noun: ")
-    noun_plural_1 = input("Noun (plural): ")
-    noun_plural_2 = input("Noun (plural): ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    noun6 = input("Noun: ")
+    noun_plural = input("Noun plural: ")
+    body_part = input("Body part: ")
+    body_part2 = input("Body part: ")
+    verb = input("Verb: ")
+    verb_past = input("Verb (past tense): ")
+    verb_past2 = input("Verb (past tense): ")
+    spell1 = input("Spell: ")
+    spell2 = input("Spell: ")
 
-    madlib = f"I love computer programming because it's {adj1}! The journey to becoming a \
-good programmer starts with hope in your mind and a dream in your {body_part}. Through blood, \
-sweat, and {noun_plural_1}, hopefully it never ends. Yes, once you learn to {verb}, it becomes \
-a part of your life identity and you can become a super {adj2} hacker. Knowledge of programming \
-lets you take control of your {noun1}. You can create your own personal {noun_plural_2}, anything \
-from developing {adj3} software to analyzing data and making predictions about the {noun2}. You can \
-maybe even recreate Jarvis and make him extra {adj4}. I hope you'll start your {adj5} journey by \
-coding with Kylie :)"
-    
+
+    madlib = f"A {adj1} glow burst suddenly across the enchanted sky above them as an edge of \
+dazzling sun appeared over the sill of the nearest {noun1}. The light hit both of their {body_part} \
+at the same time, so that Voldemort’s was suddenly a flaming {noun2}. Harry heard the high voice \
+shriek as he too {verb_past} his best hope to the heavens, pointing Draco’s {noun3}:\n\
+\"{spell1}!\"\n\
+\"{spell2}!\"\n\
+The bang was like a cannon blast, and the {adj2} flames that erupted between them, \
+at the dead center of the circle they had been treading, marked the point where the \
+{noun_plural} collided. Harry saw Voldemort’s {adj3} jet meet his own spell, saw the Elder Wand \
+fly high, dark against the sunrise, spinning across the enchanted ceiling like the \
+head of Nagini, spinning through the air toward the master it would not {verb}, who had \
+come to take full possession of it at last. And Harry, with the unerring skill of a Seeker, \
+caught the {noun4} in his free hand as Voldemort fell backward, arms splayed, the slit pupils \
+of the {adj4} {body_part2} rolling upward. Tom Riddle hit the floor with a mundane finality, his body \
+feeble and shrunken, the white hands empty, the snakelike face vacant and unknowing. Voldemort \
+was dead, {verb_past2} by his own rebounding {noun5}, and Harry stood with two wands in his hands, \
+staring down at his enemy’s {noun6}."
+
     print(madlib)
