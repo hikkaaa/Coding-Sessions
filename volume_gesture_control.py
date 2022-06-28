92a5b27db28e06b4bb05ab6a825fa7daae71a93e
unknown <francescaurgese99@gmail.com>
2022-06-28 08:16:52 -0700

    save

---
+++ volume_gesture_control.py
@@ -0,0 +1,103 @@
+import cv2
+import time
+import numpy as np
+import math
+# import library pycaw to control volume with gesture (pip install pycaw)
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+# as htm = so that we don't have to write the full name everytime 
+# we want to create an object from the class that this module contains
+import handtracking_module as htm
+
+
+
+################################################################
+wCam, hCam = 640, 480
+################################################################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(5, hCam)
+pTime = 0
+
+# create object from class HandTracker()
+detector = htm.handTracker(detectionCon=0.7)
+### initialization ###
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+######################
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+# first two parameters of GetVolumeRange() are minimum and maximum
+volRange = volume.GetVolumeRange()
+# maximum level =  100 (0)
+volume.SetMasterVolumeLevel(0, None)
+# get minimum and maximum range 
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400 
+
+while True:
+    success, img = cap.read()
+    
+    # find hand 
+    img = detector.handsFinder(img)
+    lmList = detector.positionFinder(img)
+    
+    # for this program we need are going to use the index and the thumb
+    # print only position of index and thumb
+    if len(lmList) != 0:
+        #print(lmList[4], lmList[8])
+        
+        # highlight index and thumb to make sure user is using the right fingers
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        # get the center of the line between index and thumb
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2 
+            
+        cv2.circle(img, (x1, y1), 10, (255, 0, 125), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 125), cv2.FILLED)
+        # create line between index and thumb
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 125), 3)
+        # create circle to highlight center of line 
+        cv2.circle(img, (cx, cy), 9, (255, 0, 255), cv2.FILLED)
+
+        # find the length of the line between index and thumb to change the volume based on that
+        length = math.hypot(x2-x1, y2-y1)
+        
+        # Hand range 50 - 300
+        # convert it to our volume range, that is -65 - 0
+        
+        vol = np.interp(length, [60, 300], [minVol, maxVol])
+        volBar = np.interp(length,[60, 300], [400, 150])
+        print(vol)
+        volume.SetMasterVolumeLevel(vol, None)
+        
+        # highlight minimum length
+        if length < 50:
+            cv2.circle(img, (cx, cy), 9, (0, 255, 0), cv2.FILLED)
+            
+    
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    
+    
+    
+    
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime 
+    
+    # create text box to display frames per second 
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+        (255, 0, 255), 3)
+    
+    
+    cv2.imshow("Video", img)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
